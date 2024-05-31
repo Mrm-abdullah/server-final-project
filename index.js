d4ff80f0c636e0a28a136c0ae8933238a5876905
mrmabdullah <mohammodraselmia128@gmail.com>
2024-05-31 19:52:39 +0600

    some edit

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://project-final-27f39.web.app",
+        "https://project-final-27f39.firebaseapp.com",
+    ],
+    credentials: true,
+}))
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -24,10 +31,37 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares 
+const verifyToken = (req, res, next) => {
+    // console.log('inside verify token', req.headers.authorization);
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+// use verify admin after verifyToken
+const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+    }
+    next();
+}
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db("finalProject").collection("users");
         const menuCollection = client.db("finalProject").collection("menu");
@@ -45,33 +79,6 @@ async function run() {
             res.send({ token });
         })
 
-        // middlewares 
-        const verifyToken = (req, res, next) => {
-            // console.log('inside verify token', req.headers.authorization);
-            if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'unauthorized access' });
-            }
-            const token = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if (err) {
-                    return res.status(401).send({ message: 'unauthorized access' })
-                }
-                req.decoded = decoded;
-                next();
-            })
-        }
-
-        // use verify admin after verifyToken
-        const verifyAdmin = async (req, res, next) => {
-            const email = req.decoded.email;
-            const query = { email: email };
-            const user = await userCollection.findOne(query);
-            const isAdmin = user?.role === 'admin';
-            if (!isAdmin) {
-                return res.status(403).send({ message: 'forbidden access' });
-            }
-            next();
-        }
 
         // users related api
         app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
@@ -175,7 +182,7 @@ async function run() {
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body;
             const amount = parseInt(price * 100);
-            console.log(amount, 'amount inside the intent')
+            // console.log(amount, 'amount inside the intent')
 
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
@@ -203,7 +210,7 @@ async function run() {
             const paymentResult = await paymentCollection.insertOne(payment);
 
             //  carefully delete each item from the cart
-            console.log('payment info', payment);
+            // console.log('payment info', payment);
             const query = {
                 _id: {
                     $in: payment.cartIds.map(id => new ObjectId(id))
@@ -247,7 +254,7 @@ async function run() {
 
 
         // stats or analytics
-        app.get('/admin-stats', verifyToken, verifyAdmin,  async (req, res) => {
+        app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
             const users = await userCollection.estimatedDocumentCount();
             const menuItems = await menuCollection.estimatedDocumentCount();
             const orders = await paymentCollection.estimatedDocumentCount();
@@ -302,9 +309,9 @@ async function run() {
                         as: 'menuItems'
                     }
                 },
-                    {
-                        $unwind: '$menuItems'
-                    },
+                {
+                    $unwind: '$menuItems'
+                },
                 {
                     $group: {
                         _id: '$menuItems.category',
@@ -327,8 +334,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
